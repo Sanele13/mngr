6ba71e46ba05898482fd765f72791d444daa12ba
Sanele Mpangalala <sanele.mpangalala@edgelearningmedia.com>
2019-04-02 11:20:09 +0200

    Added HttpClient on the test file, to test http service

--- angular/mngr-frontend/src/app/services/http.service.spec.ts
+++ angular/mngr-frontend/src/app/services/http.service.spec.ts
@@ -1,9 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HttpService } from './http.service';
+import {HttpClientModule} from "@angular/common/http";
 
 describe('HttpService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports:[
+      HttpClientModule
+    ],
+    providers:[
+      HttpService
+    ]}));
 
   it('should be created', () => {
     const service: HttpService = TestBed.get(HttpService);
